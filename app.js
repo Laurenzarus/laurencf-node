2449d4451f7f6f5cfe79fbf03c945d84f396060d
Laurence Fong <laurencf@usc.edu>
2019-03-05 10:30:38 -0800

    initial commit, completed part one of Node 1 assignment

---
+++ app.js
@@ -0,0 +1,71 @@
+let express = require('express');
+let knex = require('knex');
+
+let app = express();
+
+app.get('/api/genres', function(request, response) {
+  let connection = knex({
+    client: 'sqlite3',
+    connection: {
+      filename: 'chinook.db'
+    }
+  });
+
+  connection.select().from('genres').then((genres) => {
+    response.json(genres);
+  });
+});
+
+app.get('/api/genres/:id', function(request, response) {
+  let id = request.params.id;
+
+  let connection = knex({
+    client: 'sqlite3',
+    connection: {
+      filename: 'chinook.db'
+    }
+  });
+
+  connection
+    .select()
+    .from('genres')
+    .where('GenreId', id)
+    .first()
+    .then((genre) => {
+      if (genre) {
+        response.json(genre);
+      } else {
+        response.status(404).json({
+          error: `Genre ${id} not found`
+        });
+      }
+    });
+});
+
+//Node 1 Assignment
+app.get('/api/artists', function(request, response) {
+    let connection = knex({
+        client: 'sqlite3',
+        connection: {
+            filename: 'chinook.db'//can this var name be abstracted out?
+        }
+    });
+    //create query, display results
+    connection
+        .select()
+        .from('artists')
+        .then((artists) => {
+            // JSON.stringify(artists, null, 4);
+            // JSON.stringify(artists, null, "\t");
+            artists = artists.map(artist => {
+                var formattedArtist = {};
+                formattedArtist['id'] = artist['ArtistId'];
+                formattedArtist['name'] = artist['Name'];
+                return formattedArtist;
+            });
+            response.json(artists);
+        });
+});
+
+
+app.listen(process.env.PORT || 8000);
